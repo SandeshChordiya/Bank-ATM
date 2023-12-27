9c34c1c5ee0499accc87d83301e8ffee155c9272
SandeshChordiya <114577338+SandeshChordiya@users.noreply.github.com>
2023-12-27 18:50:29 +0530

    Create UpdatePhException.java
    This Exception is thrown when you have entered wrong authentication to update your phone number for your account

---
+++ UpdatePhException.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package database.operations;
+
+/**
+ *
+ * @author Sandesh
+ */
+public class UpdatePhException extends Exception{
+    public UpdatePhException(String a){
+        super(a);
+    }
+}
