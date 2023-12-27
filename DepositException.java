c03608506a51a2f1ba0942aa2b4119065318ef71
SandeshChordiya <114577338+SandeshChordiya@users.noreply.github.com>
2023-12-27 18:43:21 +0530

    Create DepositException.java
    Self defined exception for deposit function in Bank.java code

---
+++ DepositException.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package database.operations;
+
+/**
+ *
+ * @author Sandesh
+ */
+public class DepositException extends Exception{
+    public DepositException(String a){
+        super (a);
+    }  
+}
+
