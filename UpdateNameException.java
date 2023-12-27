0c665ccb4b7b095c164f62390f323deb1852ab60
SandeshChordiya <114577338+SandeshChordiya@users.noreply.github.com>
2023-12-27 18:49:26 +0530

    Create UpdateNameException.java
    This Exception is Thrown while authenticating for update of the name for your bank account

---
+++ UpdateNameException.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package database.operations;
+
+/**
+ *
+ * @author Sandesh
+ */
+public class UpdateNameException extends Exception{
+    public UpdateNameException(String args)
+    {
+        super(args);
+    }
+}
