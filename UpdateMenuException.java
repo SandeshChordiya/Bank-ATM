1b0dbf30b3a477ee074af125e804a9dd67d0e8f9
SandeshChordiya <114577338+SandeshChordiya@users.noreply.github.com>
2023-12-27 18:47:55 +0530

    Create UpdateMenuException.java
    Exception to handle Update Menu : if the user choses wrong option from the menu then this Exception will be thrown

---
+++ UpdateMenuException.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package database.operations;
+
+/**
+ *
+ * @author Sandesh
+ */
+public class UpdateMenuException extends Exception{
+    public UpdateMenuException (String a){
+        super (a);
+    }
+}
