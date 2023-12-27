6630bbc53fcd855429561b95e1e7f518bfcff54b
SandeshChordiya <114577338+SandeshChordiya@users.noreply.github.com>
2023-12-27 18:45:07 +0530

    Create WithdrawException.java
    self created Exception for Withdrawing Money in Bank.java file

---
+++ WithdrawException.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package database.operations;
+
+/**
+ *
+ * @author Sandesh
+ */
+public class WithdrawException extends Exception{
+    public WithdrawException(String a)
+    {
+        super (a);
+    }
+}
